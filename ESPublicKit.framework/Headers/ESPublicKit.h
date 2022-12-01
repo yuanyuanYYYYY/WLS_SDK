4eca2cb4069720ca874fb25cfeb28af828d6e335
yuanyuanYYYYY <119490538+yuanyuanYYYYY@users.noreply.github.com>
2022-12-01 14:59:57 +0800

    Add files via upload

---
+++ ESPublicKit.framework/Headers/ESPublicKit.h
@@ -0,0 +1,43 @@
+//
+//  ESUIKit.h
+//  ESUIKit
+//
+//  Created by esunny on 2020/8/25.
+//  Copyright © 2020 esunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <ESPublicKit/EsDataConstant.h>
+#import <ESPublicKit/EStarApi_TYPE.h>
+#import <ESPublicKit/ESQuoteClass.h>
+#import <ESPublicKit/ESOtherClass.h>
+#import <ESPublicKit/ESQuoteClass.h>
+#import <ESPublicKit/EsDataApi.h>
+
+#import <ESPublicKit/ESTradeLoginApiManager.h>
+#import <ESPublicKit/ESTradeMessageApiManager.h>
+#import <ESPublicKit/ESTradeAboutManager.h>
+#import <ESPublicKit/ESQuoteApiManager.h>
+#import <ESPublicKit/ESTradeApiManager.h>
+#import <ESPublicKit/ESTradeNetApiManager.h>
+#import <ESPublicKit/ESEventApiManager.h>
+
+#import <ESPublicKit/ESStartup.h>
+#import <ESPublicKit/ESUIApi.h>
+#import <ESPublicKit/ESKLineViewController.h>
+#import <ESPublicKit/ESRootViewController.h>
+#import <ESPublicKit/ESRootViewController.h>
+#import <ESPublicKit/ESBaseNavigationController.h>
+#import <ESPublicKit/ESQuoteBaseController.h>
+#import <ESPublicKit/ESQuoteMainController.h>
+#import <ESPublicKit/ESFavoriteController.h>
+
+//! Project version number for ESUIKit.
+FOUNDATION_EXPORT double ESUIKitVersionNumber;
+
+//! Project version string for ESUIKit.
+FOUNDATION_EXPORT const unsigned char ESUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESUIKit/PublicHeader.h>
+
+
